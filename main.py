aa13eb08a990528444eb81047f0a063668aa43b3
FIVE <cahhong@artfx.fr>
2024-07-23 23:20:16 +0200

    Making everything work

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 import uuid
 import shutil
 import json
+from unidecode import unidecode
 import zdubdeepl as deepl
 import zdubelevenlabs as elevenlabs
 import zdubwhisper as whisper
@@ -22,35 +23,49 @@ shutil.copy(video_path, f'{project_folder}/original.mp4')
 #convert the video to audio
 os.system(f'ffmpeg -i {project_folder}/original.mp4 -vn {project_folder}/original.wav')
 
+
 #WHISPER TRANSCRIPTION 
 transcription = whisper.transcription(f'{project_folder}/original.wav')
-print(transcription)
 
-extract = transcription['segments']
-
+transcription = transcription['segments']
 # dict to json file
 with open(f'{project_folder}/transcription.json', 'w') as transcription_file:
-    json.dump(extract, transcription_file)
+    json.dump(transcription, transcription_file)
 
+langs = ['FR', 'ES', 'IT',]
 
-'''
-#TRANSLATE THE TRANSCRIPTION 
-lang = 'FR'
-lang_folder = os.path.join(project_folder, lang)
-os.makedirs(lang_folder, exist_ok=True)
+for lang in langs:
+    #TRANSLATE THE TRANSCRIPTION 
+    lang_folder = os.path.join(project_folder, lang)
+    os.makedirs(lang_folder, exist_ok=True)
 
-translation = deepl.translate('Hello World', lang)
-print(translation)
+    lang_transcription = transcription
+    for segment in lang_transcription:
+        translation = deepl.translate(segment['text'], lang)
+        segment['text'] = unidecode(str(translation))
+    
+    # dict to json file
+    with open(f'{lang_folder}/transcription.json', 'w') as transcription_file:
+        json.dump(lang_transcription, transcription_file)
+    
+for lang in langs:
+    lang_folder = os.path.join(project_folder, lang)
+    with open(f'{lang_folder}/transcription.json', 'r') as json_file:
+        lang_transcription = json.load(json_file)
+    
+    count = 0
+    audio_folder = os.path.join(lang_folder, 'audio_files')
+    os.makedirs(audio_folder, exist_ok=True)
+    
+    for segment in lang_transcription:
+        count += 1
+        voiceid = 'XrExE9yKIg1WjnnlVkGX'
 
-#GENERATE THE AUDIO FILE
-version = 1
-voiceid = 'pFZP5JQG7iQjIQuC4Bku'
-audio_folder = os.path.join(lang_folder, 'audio_files')
-os.makedirs(audio_folder, exist_ok=True)
+        text = unidecode(str(segment['text']))
 
-audio = elevenlabs.audio(translation, voiceid)
-with open(f'{audio_folder}/elevenlabs_{lang}_{str(version).zfill(5)}.mp3', 'wb') as audio_file:
+        audio = elevenlabs.audio(text, voiceid, (lang.lower()))
+        with open(f'{audio_folder}/elevenlabs_{lang}_{str(count).zfill(5)}.mp3', 'wb') as audio_file:
             audio_file.write(audio)
-'''
-
+        
+        
 #MIX THE AUDIO TO THE FILE (FFMEG ??? )
--- zdubelevenlabs.py
+++ zdubelevenlabs.py
@@ -1,29 +1,35 @@
-def audio(text, voiceid):
+def audio(text, voiceid, lang):
     import requests
+    import time
+    code = 0
 
-    # Ensure text is a string
-    if not isinstance(text, str):
-        text = str(text)
+    for i in range(10):
+        # Ensure text is a string
+        if not isinstance(text, str):
+            text = str(text)
 
-    #get authkey from a file
-    with open('elevenlabs.secret', 'r') as file:
-        authkey = file.read().replace('\n', '')
-    
-    url = f"https://api.elevenlabs.io/v1/text-to-speech/{voiceid}"
-    
-    payload = {
-        "text": text,
-    }
-    
-    headers = { 
-        "xi-api-key": authkey,
-        "Content-Type": "application/json"
-    }
-    
-    response = requests.request("POST", url, json=payload, headers=headers)
-    
-    if response.status_code==200:
-        return(response.content)
-    else:
-        print(response.status_code)
-        print(response.text)
+        #get authkey from a file
+        with open('elevenlabs.secret', 'r') as file:
+            authkey = file.read().replace('\n', '')
+        
+        url = f"https://api.elevenlabs.io/v1/text-to-speech/{voiceid}"
+        
+        payload = {
+            "text": text,
+            "model_id": "eleven_turbo_v2_5",
+            "language_code": lang
+        }
+        
+        headers = { 
+            "xi-api-key": authkey,
+            "Content-Type": "application/json"
+        }
+        
+        response = requests.request("POST", url, json=payload, headers=headers)
+        
+        if response.status_code==200:
+            return(response.content)
+        elif i==0:
+            print(response.status_code)
+            print(response.text)
+            time.sleep(1)
